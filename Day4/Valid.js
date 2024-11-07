0e81a6e83afedd4f33895bda7cc2c29f4bce6e4e
dar2005 <darshikagrover2005@gmail.com>
2024-11-07 23:57:41 +0530

    Day4

---
+++ Day4/Valid.js
@@ -0,0 +1,7 @@
+function validated(){
+    let username=document.getElementById("un").value;
+    let pass=document.getElementById("pass").value;
+    if(username.length<10)
+        return false;
+    if(password.length>=6)
+}
